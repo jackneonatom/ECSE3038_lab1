e266abee1ca66d531488e0999180de53c1480efc
Nathanm <122718848+jackneonatom@users.noreply.github.com>
2024-02-01 23:50:25 -0500

    silly silly

--- labone.py
+++ labone.py
@@ -1,29 +1,30 @@
 
 def parallel(resistors):
     if len(resistors) < 2:
-        return "At least 2 resistors are required for parallel connection"
+        return "silly silly thatss not parallel"
     else:
         total_inverse = sum(1 / r for r in resistors)
         equivalent_resistance = 1 / total_inverse
-        return f"{equivalent_resistance:.3f} ohms"
+        return f"{equivalent_resistance} ohms"
 
 
 def potential_divider(vin, Rvals):
     RT = sum(Rvals)
     vol_drops = [vin * (r / RT) for r in Rvals]
-    print(vol_drops,"v")
-    return 
+    # print(vol_drops,"v")
+    return f"{vol_drop} v"
 
 
-def temperature_check(temp, unit):
-    if unit.lower() == 'c':
+
+def temperature_check(temp, type):
+    if type.lower() == 'c':
         if temp < 36.5:
             return "The patient is hypothermic"
         elif temp > 37.5:
             return "The patient is hyperthermic"
         else:
             return "The patient has a normal body temperature"
-    elif unit.lower() == 'f':
+    elif type.lower() == 'f':
         temp_celsius = (temp - 32) * 5/9
         if temp_celsius < 36.5:
             return "The patient is hypothermic"
@@ -32,7 +33,7 @@ def temperature_check(temp, unit):
         else:
             return "The patient has a normal body temperature"
     else:
-        return "Please provide a valid unit (C for Celsius, F for Fahrenheit)"
+        return "silly silly, thats not righttt"
 
 
 
