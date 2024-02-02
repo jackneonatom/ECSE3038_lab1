e8600dc518f4094089e01c762ea20e2d96fe62dc
Nathanm <122718848+jackneonatom@users.noreply.github.com>
2024-02-01 23:20:39 -0500

    cantboda

--- labone.py
+++ labone.py
@@ -1,25 +1,38 @@
-# def parallel(resistors):
-#     for r in (resistors):
-#             total_inverse = sum(1 / r)
-#             equivalent_resistance = 1 / total_inverse
-#             print(equivalent_resistance)
-#             return 
 
-#             parallel([3,4,5])
+def parallel(resistors):
+    if len(resistors) < 2:
+        return "At least 2 resistors are required for parallel connection"
+    else:
+        total_inverse = sum(1 / r for r in resistors)
+        equivalent_resistance = 1 / total_inverse
+        return f"{equivalent_resistance:.3f} ohms"
 
-def parallel(r):
-    count=0
-    eqr=0
-    for i in r:
-        count=count+1
-        while count!=len(r):
-            
-            total=1/i
-            a=total
-            b=total+a
+
+def potential_divider(vin, Rvals):
+    RT = sum(Rvals)
+    vol_drops = [vin * (r / RT) for r in Rvals]
+    print(vol_drops,"v")
+    return 
+
+
+def temperature_check(temp, unit):
+    if unit.lower() == 'c':
+        if temp < 36.5:
+            return "The patient is hypothermic"
+        elif temp > 37.5:
+            return "The patient is hyperthermic"
         else:
-            print(" ")
-    return total
-parallel([330, 1000, 2200])
+            return "The patient has a normal body temperature"
+    elif unit.lower() == 'f':
+        temp_celsius = (temp - 32) * 5/9
+        if temp_celsius < 36.5:
+            return "The patient is hypothermic"
+        elif temp_celsius > 37.5:
+            return "The patient is hyperthermic"
+        else:
+            return "The patient has a normal body temperature"
+    else:
+        return "Please provide a valid unit (C for Celsius, F for Fahrenheit)"
+temperature_check(14, "C")
 
-#number 2
+
