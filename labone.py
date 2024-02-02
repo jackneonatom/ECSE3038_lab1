845e235f15437c89fea3fa37d7f47915e38d22f5
Nathanm <122718848+jackneonatom@users.noreply.github.com>
2024-02-01 22:42:05 -0500

    hi

--- labone.py
+++ labone.py
@@ -1,2 +1,22 @@
-print("hello world")
-# hi how are you
+# def parallel(resistors):
+#     for r in (resistors):
+#             total_inverse = sum(1 / r)
+#             equivalent_resistance = 1 / total_inverse
+#             print(equivalent_resistance)
+#             return 
+
+#             parallel([3,4,5])
+
+def parallel(r):
+    count=0
+    eqr=0
+    for i in r:
+        count=count+1
+        if count==len(r):
+            
+            total=1/sum(i)
+            eqr=1/total
+        else:
+            print(" ")
+    return total
+parallel([330, 1000, 2200])
